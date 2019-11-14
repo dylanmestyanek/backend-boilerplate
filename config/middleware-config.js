7942a014c5a129b60a281aacebc41bd59c424eb0
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-13 19:04:50 -0600

    removed morgan package

--- config/middleware-config.js
+++ config/middleware-config.js
@@ -2,5 +2,4 @@ module.exports = server => {
     server.use(require("express").json());
     server.use(require("helmet")());
     server.use(require("cors")());
-    server.use(require("morgan")("dev"));
 }
